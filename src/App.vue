9a0da3efd65a63c8d60787f565a750cdac5ba43e
Oluwadamilare Abiodun <dami@appyclan.com>
2024-01-02 06:45:22 +0100

    Add UI and applicable functions

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,110 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref, onMounted, computed, watch } from "vue";
+
+  const todos = ref([])
+  const name = ref('')
+  const input_content = ref('')
+  const input_category = ref(null)
+  const todos_asc = computed(
+      () => todos.value.sort((a,b) => {
+        return b.createdAt - a.createdAt
+      })
+  )
+
+  const addTodo = () => {
+      if (input_content.value.trim() === '' || input_category.value === null){
+          return
+      }
+
+      todos.value.push({
+        content: input_content.value,
+        category: input_category.value,
+        done: false,
+        createdAt: new Date().getTime()
+      })
+
+    input_content.value = ''
+    input_category.value = null
+  }
+
+  const removeTodo = todo => {
+    todos.value = todos.value.filter(t => t !== todo)
+  }
+
+  watch(todos, (newVal) => {
+    localStorage.setItem('todos',JSON.stringify(newVal))
+  }, {deep:true})
+
+  watch(name, (newVal) => {
+    localStorage.setItem('name',newVal)
+  })
+
+  onMounted(
+      () => {
+        name.value = localStorage.getItem('name') || ''
+        todos.value = JSON.parse(localStorage.getItem('todos')) || []
+      }
+  )
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        Hello <input type="text" placeholder="Name here" v-model="name">
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3 class="title">CREATE TODO ITEMS</h3>
+      <form @submit.prevent="addTodo">
+        <div>
+          <h4>What's on your to-do list></h4>
+          <input type="text" placeholder="e.g read a book" v-model="input_content">
+        </div>
+        <div>
+          <h4>Pick a category</h4>
+          <div class="options">
+            <label>
+              <input type="radio" name="category" value="business" v-model="input_category" />
+              <span class="bubble business"></span>
+              <span class="options-text">Business</span>
+            </label>
+
+            <label>
+              <input type="radio" name="category" value="personal" v-model="input_category" />
+              <span class="bubble personal"></span>
+              <span class="options-text">Personal</span>
+            </label>
+          </div>
+        </div>
+        <div>
+          <input type="submit" value="Add todo">
+        </div>
+      </form>
+    </section>
+
+    <section class="todo-list">
+      <h3 class="title">TODO LIST</h3>
+      <div class="list">
+        <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model="todo.done">
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content">
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
